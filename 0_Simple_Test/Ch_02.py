b869e651fd67461a80cf7f7de9e36db34e8e68b3
Ai-BT <aiart3536@gmail.com>
2022-06-12 23:06:04 +0900

    간단한 map 사용

--- 0_Simple_Test/Ch_02.py
+++ 0_Simple_Test/Ch_02.py
@@ -111,3 +111,115 @@ hello('김다정')
 
 
 # %%
+
+# 6. 로꾸거
+# 입력 거꾸로 // 출력 로꾸꺼
+
+word = '로꾸거'
+revers_word = "".join(reversed(word))
+
+print(revers_word)
+print(word[::-1])
+
+s = 'abcde'
+print(s[3::-1]) # dcba
+print(s[4::-1])  # edcba
+print(s[::-1])  # edcba
+
+# %%
+
+# 7.
+# 유주는 놀이공원 아르바이트 중입니다. 그런데 놀이기구마다 키 제한이 있습니다.
+# 유주가 담당하는 놀이기구는 키가 150 이상만 탈 수 있습니다.
+
+# 입력으로 키가 주어지면
+# 키가 150이 이상이면 YES를 틀리면 NO를 출력하는 프로그램을 작성하세요.
+
+
+def play(tall):
+    if tall >= 150:
+        print("Yes")
+    elif tall < 150:
+        print("No")
+
+play(149)
+
+
+# %%
+
+# 8.
+# 영하네 반은 국어, 수학, 영어 시험을 보았습니다. 영하는 친구들의 평균 점수를 구해주기로 했습니다.
+
+# 공백으로 구분하여 세 과목의 점수가 주어지면
+# 전체 평균 점수를 구하는 프로그램을 작성하세요. 단, 소숫점 자리는 모두 버립니다.
+
+def score(kor, math, eng):
+    avg = (kor + math + eng) / 3
+    result = round(avg, 0)
+    print(kor, math, eng, sep=" ")
+    print(result)
+
+score(94.2, 44, 20.44)
+
+data = list(map(int, input().split()))
+print(int(sum(data)/3))
+ 
+
+# %%
+
+
+# 첫 번째 매개변수로는 함수가 오고
+# 두 번째 매개변수로는 반복 가능한 자료형(리스트, 튜플 등)이 옵니다
+
+# map 함수의 반환 값은 map객체 이기 때문에 
+# 해당 자료형을 list 혹은 tuple로 형 변환시켜주어야 합니다.
+
+
+# 리스트에 값을 하나씩 더해서 새로운 리스트를 만드는 작업
+myList = [1, 2, 3, 4, 5]
+
+# for 반복문 이용
+result1 = []
+for val in myList:
+    result1.append(val + 1)
+
+print(f'result1 : {result1}')
+
+
+# map 함수 이용
+def add_one(n):
+    return n + 1
+
+
+result2 = list(map(add_one, myList))  # map반환을 list 로 변환
+print(f'result2 : {result2}')
+
+
+# %%
+
+# 9. 공백으로 구분하여 두 숫자 a와 b가 주어지면, a의 b승을 구하는 프로그램을 작성하세요.
+
+def math(a,b):
+    return a**b
+
+print(math(3,3))
+
+
+n = list(map(int, input().split()))
+print(n[0] ** n[1])
+
+
+# %%
+
+# 10. 공백으로 구분하여 두 숫자가 주어집니다.
+# 첫번째 숫자로 두번째 숫자를 나누었을 때 그 몫과 나머지를 공백으로 구분하여 출력하세요
+
+data = list(map(int, input().split()))
+
+result = data[0] // data[1]
+left = data[0] % data[1]
+
+print(result, left)
+
+
+# %%
