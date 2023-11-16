5710d23299c725d3e880354e4b00d0894c03f9a1
Ai-BT <aiart3536@gmail.com>
2023-11-16 22:42:27 +0900

    백준

---
+++ 1_baekjoon/lv_01.py
@@ -0,0 +1,37 @@
+# %%
+# 10171
+
+print("\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \(__)|")
+
+
+# %%
+# 10172
+
+print("|\_/|")
+print("|q p|   /}")
+print('( 0 )"""\\')
+print('|"^"`    |')
+print("||_/=\\\__|")
+
+
+# %%
+# 1000
+
+a, b = map(int, input().split())
+print(a+b)
+# %%
+# 10926
+
+name = input(str)
+print("{}??!".format(name))
+
+print(input() + "??!")
+
+# %%
+# 18108
+
+print(int(input()) - 543)
+
