fb7d41671f846993f37e49576d2898494ac71761
Ai-BT <aiart3536@gmail.com>
2024-06-04 22:12:45 +0900

    test.py

--- 3_programmers/test.py
+++ 3_programmers/test.py
@@ -1,47 +1,29 @@
-# 문제 4
-# 수포자는 수학을 포기한 사람을 줄인 표현입니다.
-# 수포자 삼인방은 모의고사에 수학 문제를 전부 찍으려 합니다.
-# 수포자는 1번 문제부터 마지막 문제까지 다음과 같이 찍습니다.
 
-# 1번 수포자가 찍는 방식 : 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
-# 2번 수포자가 찍는 방식 : 2, 1, 2, 3, 2, 4, 2, 5 ...
-# 3번 수포자가 찍는 방식 : 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 ...
-# 1번 문제부터 마지막 문제까지의 정답이 순서대로 저장된 배열 answers가 주어졌을 때 가장 많은 문제를
-# 맞힌 사람이 누구인지 배열에 담아 반환하도록 solution( ) 함수를 작성하세요.
+# 문제 15 요세푸스 문제
+# N명의 사람이 원 형태로 서 있습니다. 각 사람은 1부터 N까지 번호표를 갖고 있습니다.
+# 그리소 임의의 숫자 k가 주어졌을 때 다음과 같이 사람을 없앱니다.
 
-# answers = [1,2,3,4,5]
-answers = [1, 3, 2, 4, 2]
+# - 1번 번호표를 가진 사람을 기준으로 K 번째 사람을 없앱니다.
+# - 없앤 사람 다음 사람을 기준으로 하고 다시 k 번째 사람을 없앱니다.
+
+# n 과 k 가 주어질 떄 마지막에 살아있는 사람의 번호를 반환하는 함수를 구현해주세요.
+
+# 입출력 예
+# n k reture
+# 5 2 3
+
+from collections import deque
 
 
-patterns = [
-    [1, 2, 3, 4, 5],
-    [2, 1, 2, 3, 2, 4, 2, 5],
-    [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-]
+def solution(N, K):
+    # ❶ 1부터 N까지의 번호를 deque에 추가
+    queue = deque(range(1, N+1))
 
-# 1. 답안과 제출한 정답이 일치하는지 확인
+    while len(queue) > 1:  # ❷ deque에 하나의 요소가 남을 때까지
+        for _ in range(K-1):
+            queue.append(queue.popleft())  # ❸ K번째 요소를 찾기 위해 앞에서부터 제거하고 뒤에 추가
+        queue.popleft()  # ❹ K번째 요소 제거
+    return queue[0]  # ❺ 마지막으로 남은 요소 반환
 
-scores = [0] * 3
 
-for i, answer in enumerate(answers):
-    for j, pattern in enumerate(patterns):
-        if answer == pattern[i % len(pattern)]:
-            scores[j] += 1
-
-print("socres = ", scores)
-
-# 2. max score 구하기
-
-max_score = max(scores)
-
-print("max = ", max_score)
-
-# 3. 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담기
-
-highest_score = []
-
-for i, score in enumerate(scores):
-    if score == max_score:
-        highest_score.append(i + 1)
-
-print(highest_score)
+print(solution(5, 2))
