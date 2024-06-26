cda89e898d297e05ddc993f290e9a4fa46666ba9
Daehwan <aiart3536@gmail.com>
2024-06-26 21:18:28 +0900

    kaggle

---
+++ kaggle_Spaceship.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+# import tensorflow_decision_forests as tfdf
+import pandas as pd
+import numpy as npe
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+print("TensorFlow v" + tf.__version__)
